blackden/xray-16

commit 1db4fe7e634735b45a084cc4eb43921794384ba8

Support SOC HUD animations (#392)

src/xrGame/WeaponKnife.cpp
@@ -46,6 +46,9 @@ CWeaponKnife::CWeaponKnife()
     m_Splash2HitsCount = 2;
 
     m_NextHitDivideFactor = 0.75f;
+
+    oldStrikeMethod = false;
+    hitEndAnimation = false;
 }
 
 enum class FieldTypes
@@ -321,7 +324,7 @@ void CWeaponKnife::OnMotionMark(u32 state, const motion_marks& M)
     p1.set(get_LastFP());
     d.set(get_LastFD());
 
-    if (H_Parent())
+    if (H_Parent() && !hitEndAnimation)
     {
         smart_cast<CEntity*>(H_Parent())->g_fireParams(this, p1, d);
         KnifeStrike(p1, d);
@@ -335,8 +338,24 @@ void CWeaponKnife::OnAnimationEnd(u32 state)
     case eHiding: SwitchState(eHidden); break;
 
     case eFire:
-    case eFire2: SwitchState(eIdle); break;
-
+    case eFire2:
+    {
+        if (attackStarted)
+        {
+            attackStarted = false;
+            if (state == eFire)
+                PlayHUDMotion("anm_attack_end", "anim_shoot1_end", FALSE, this, state);
+            else // eFire2
+                PlayHUDMotion("anm_attack2_end", "anim_shoot2_end", FALSE, this, state);
+            hitEndAnimation = true;
+        }
+        else
+        {
+            hitEndAnimation = false;
+            SwitchState(eIdle);
+        }
+        break;
+    }
     case eShowing:
     case eIdle: SwitchState(eIdle); break;
 
@@ -351,10 +370,11 @@ void CWeaponKnife::switch2_Attacking(u32 state)
         return;
 
     if (state == eFire)
-        PlayHUDMotion("anm_attack", FALSE, this, state);
+        PlayHUDMotion("anm_attack", "anim_shoot1_start", FALSE, this, state);
     else // eFire2
-        PlayHUDMotion("anm_attack2", FALSE, this, state);
+        PlayHUDMotion("anm_attack2", "anim_shoot2_start", FALSE, this, state);
 
+    attackStarted = true;
     SetPending(TRUE);
 }
 
@@ -370,7 +390,7 @@ void CWeaponKnife::switch2_Hiding()
 {
     FireEnd();
     VERIFY(GetState() == eHiding);
-    PlayHUDMotion("anm_hide", TRUE, this, GetState());
+    PlayHUDMotion("anm_hide", "anim_hide", TRUE, this, GetState());
 }
 
 void CWeaponKnife::switch2_Hidden()
@@ -382,7 +402,7 @@ void CWeaponKnife::switch2_Hidden()
 void CWeaponKnife::switch2_Showing()
 {
     VERIFY(GetState() == eShowing);
-    PlayHUDMotion("anm_show", FALSE, this, GetState());
+    PlayHUDMotion("anm_show", "anim_draw", FALSE, this, GetState());
 }
 
 void CWeaponKnife::FireStart()

src/xrGame/Artefact.cpp
@@ -415,16 +415,16 @@ void CArtefact::OnStateSwitch(u32 S, u32 oldState)
     inherited::OnStateSwitch(S, oldState);
     switch (S)
     {
-    case eShowing: { PlayHUDMotion("anm_show", FALSE, this, S);
+    case eShowing: { PlayHUDMotion("anm_show", "anim_show", FALSE, this, S);
     }
     break;
     case eHiding:
     {
         if (oldState != eHiding)
-            PlayHUDMotion("anm_hide", FALSE, this, S);
+            PlayHUDMotion("anm_hide", "anim_hide", FALSE, this, S);
     }
     break;
-    case eActivating: { PlayHUDMotion("anm_activate", FALSE, this, S);
+    case eActivating: { PlayHUDMotion("anm_activate", "anim_activate", FALSE, this, S);
     }
     break;
     case eIdle: { PlayAnimIdle();
@@ -433,7 +433,7 @@ void CArtefact::OnStateSwitch(u32 S, u32 oldState)
     };
 }
 
-void CArtefact::PlayAnimIdle() { PlayHUDMotion("anm_idle", FALSE, NULL, eIdle); }
+void CArtefact::PlayAnimIdle() { PlayHUDMotion("anm_idle", "anim_idle", FALSE, NULL, eIdle); }
 void CArtefact::OnAnimationEnd(u32 state)
 {
     switch (state)

src/xrGame/CustomDetector.cpp
@@ -127,7 +127,7 @@ void CCustomDetector::OnStateSwitch(u32 S, u32 oldState)
     {
         g_player_hud->attach_item(this);
         m_sounds.PlaySound("sndShow", Fvector().set(0, 0, 0), this, true, false);
-        PlayHUDMotion(m_bFastAnimMode ? "anm_show_fast" : "anm_show", FALSE /*TRUE*/, this, GetState());
+        PlayHUDMotion(m_bFastAnimMode ? "anm_show_fast" : "anm_show", "anim_show", FALSE /*TRUE*/, this, GetState());
         SetPending(TRUE);
     }
     break;
@@ -136,7 +136,7 @@ void CCustomDetector::OnStateSwitch(u32 S, u32 oldState)
         if (oldState != eHiding)
         {
             m_sounds.PlaySound("sndHide", Fvector().set(0, 0, 0), this, true, false);
-            PlayHUDMotion(m_bFastAnimMode ? "anm_hide_fast" : "anm_hide", FALSE/*TRUE*/, this, GetState());
+            PlayHUDMotion(m_bFastAnimMode ? "anm_hide_fast" : "anm_hide", "anim_show", FALSE/*TRUE*/, this, GetState());
             SetPending(TRUE);
         }
     }

src/xrGame/HudItem.cpp
@@ -146,7 +146,7 @@ void CHudItem::OnAnimationEnd(u32 state)
     }
 }
 
-void CHudItem::PlayAnimBore() { PlayHUDMotion("anm_bore", TRUE, this, GetState()); }
+void CHudItem::PlayAnimBore() { PlayHUDMotion("anm_bore", "anim_idle", TRUE, this, GetState()); }
 bool CHudItem::ActivateItem()
 {
     OnActiveItem();
@@ -275,6 +275,18 @@ u32 CHudItem::PlayHUDMotion(const shared_str& M, BOOL bMixIn, CHudItem* W, u32 s
     return anim_time;
 }
 
+u32 CHudItem::PlayHUDMotion(const shared_str& M, const shared_str& M2, BOOL bMixIn, CHudItem* W, u32 state)
+{
+    u32 time = 0;
+
+    if (isHUDAnimationExist(M.c_str()))
+        time = PlayHUDMotion(M, bMixIn, W, state);
+    else if (isHUDAnimationExist(M2.c_str()))
+        time = PlayHUDMotion(M2, bMixIn, W, state);
+
+    return time;
+}
+
 u32 CHudItem::PlayHUDMotion_noCB(const shared_str& motion_name, BOOL bMixIn)
 {
     m_current_motion = motion_name;
@@ -320,7 +332,7 @@ void CHudItem::PlayAnimIdle()
     if (TryPlayAnimIdle())
         return;
 
-    PlayHUDMotion("anm_idle", TRUE, NULL, GetState());
+    PlayHUDMotion("anm_idle", "anim_idle", TRUE, NULL, GetState());
 }
 
 bool CHudItem::TryPlayAnimIdle()
@@ -377,9 +389,9 @@ bool CHudItem::isHUDAnimationExist(pcstr anim_name)
     return false;
 }
 
-void CHudItem::PlayAnimIdleMovingCrouch() { PlayHUDMotion("anm_idle_moving_crouch", true, nullptr, GetState()); }
-void CHudItem::PlayAnimIdleMoving() { PlayHUDMotion("anm_idle_moving", true, nullptr, GetState()); }
-void CHudItem::PlayAnimIdleSprint() { PlayHUDMotion("anm_idle_sprint", true, nullptr, GetState()); }
+void CHudItem::PlayAnimIdleMovingCrouch() { PlayHUDMotion("anm_idle_moving_crouch", "anim_idle", true, nullptr, GetState()); }
+void CHudItem::PlayAnimIdleMoving() { PlayHUDMotion("anm_idle_moving", "anim_idle", true, nullptr, GetState()); }
+void CHudItem::PlayAnimIdleSprint() { PlayHUDMotion("anm_idle_sprint", "anim_idle", true, nullptr, GetState()); }
 void CHudItem::OnMovementChanged(ACTOR_DEFS::EMoveCommand cmd)
 {
     if (GetState() == eIdle && !m_bStopAtEndAnimIsRunning)

src/xrGame/HudItem.h
@@ -128,6 +128,7 @@ public:
     virtual void UpdateXForm() = 0;
 
     u32 PlayHUDMotion(const shared_str& M, BOOL bMixIn, CHudItem* W, u32 state);
+    u32 PlayHUDMotion(const shared_str& M, const shared_str& M2, BOOL bMixIn, CHudItem* W, u32 state);
     u32 PlayHUDMotion_noCB(const shared_str& M, BOOL bMixIn);
     void StopCurrentAnimWithoutCallback();
 

src/xrGame/Missile.cpp
@@ -246,7 +246,7 @@ void CMissile::State(u32 state, u32 oldState)
     case eShowing:
     {
         SetPending(TRUE);
-        PlayHUDMotion("anm_show", FALSE, this, GetState());
+        PlayHUDMotion("anm_show", "anim_show", FALSE, this, GetState());
     }
     break;
     case eIdle:
@@ -262,7 +262,7 @@ void CMissile::State(u32 state, u32 oldState)
             if (oldState != eHiding)
             {
                 SetPending(TRUE);
-                PlayHUDMotion("anm_hide", TRUE, this, GetState());
+                PlayHUDMotion("anm_hide", "anim_hide", TRUE, this, GetState());
             }
         }
     }
@@ -286,20 +286,23 @@ void CMissile::State(u32 state, u32 oldState)
     {
         SetPending(TRUE);
         m_fThrowForce = m_fMinForce;
-        PlayHUDMotion("anm_throw_begin", TRUE, this, GetState());
+        PlayHUDMotion("anm_throw_begin", "anim_throw_begin", TRUE, this, GetState());
     }
     break;
-    case eReady: { PlayHUDMotion("anm_throw_idle", TRUE, this, GetState());
+    case eReady: { PlayHUDMotion("anm_throw_idle", "anim_throw_idle", TRUE, this, GetState());
     }
     break;
     case eThrow:
     {
         SetPending(TRUE);
         m_throw = false;
-        PlayHUDMotion("anm_throw", TRUE, this, GetState());
+        PlayHUDMotion("anm_throw", "anim_throw_act", TRUE, this, GetState());
     }
     break;
-    case eThrowEnd: { SwitchState(eShowing);
+    case eThrowEnd:
+    {
+        PlayHUDMotion("anm_throw_end", "anim_throw_end", TRUE, this, GetState());
+        SwitchState(eShowing);
     }
     break;
         /*	case eBore:

src/xrGame/WeaponAutomaticShotgun.cpp
@@ -154,18 +154,18 @@ void CWeaponAutomaticShotgun::switch2_EndReload()
 void CWeaponAutomaticShotgun::PlayAnimOpenWeapon()
 {
     VERIFY(GetState() == eReload);
-    PlayHUDMotion("anm_open", FALSE, this, GetState());
+    PlayHUDMotion("anm_open", "anim_open", FALSE, this, GetState());
 }
 void CWeaponAutomaticShotgun::PlayAnimAddOneCartridgeWeapon()
 {
     VERIFY(GetState() == eReload);
-    PlayHUDMotion("anm_add_cartridge", FALSE, this, GetState());
+    PlayHUDMotion("anm_add_cartridge", "anim_add_cartridge", FALSE, this, GetState());
 }
 void CWeaponAutomaticShotgun::PlayAnimCloseWeapon()
 {
     VERIFY(GetState() == eReload);
 
-    PlayHUDMotion("anm_close", FALSE, this, GetState());
+    PlayHUDMotion("anm_close", "anim_close", FALSE, this, GetState());
 }
 
 bool CWeaponAutomaticShotgun::HaveCartridgeInInventory(u8 cnt)

src/xrGame/WeaponKnife.h
@@ -28,6 +28,8 @@ protected:
     u16 knife_material_idx;
 
 protected:
+    bool attackStarted;
+    bool hitEndAnimation;
     bool oldStrikeMethod;
 
     ALife::EHitType m_eHitType;

commit 318bd080d6b7ac20f0086de0dee8f1e0e7e49a85

xrGame/WeaponKnife.cpp: fixed broken knife
Was broken with 1db4fe7e634735b45a084cc4eb43921794384ba8

src/xrGame/WeaponKnife.cpp
@@ -340,16 +340,17 @@ void CWeaponKnife::OnAnimationEnd(u32 state)
     case eFire:
     case eFire2:
     {
+        u32 time = 0;
         if (attackStarted)
         {
             attackStarted = false;
             if (state == eFire)
-                PlayHUDMotion("anm_attack_end", "anim_shoot1_end", FALSE, this, state);
+                time = PlayHUDMotion("anm_attack_end", "anim_shoot1_end", FALSE, this, state);
             else // eFire2
-                PlayHUDMotion("anm_attack2_end", "anim_shoot2_end", FALSE, this, state);
-            hitEndAnimation = true;
+                time = PlayHUDMotion("anm_attack2_end", "anim_shoot2_end", FALSE, this, state);
+            hitEndAnimation = time != 0;
         }
-        else
+        if (time == 0)
         {
             hitEndAnimation = false;
             SwitchState(eIdle);