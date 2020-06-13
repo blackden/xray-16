blackden/xray-16

commit 0cd8124fd1f1008200433bbf2c09c028a94e1ffd

Fixed display on HUD of transparent surfaces. Untested!

src/Layers/xrRender/r__dsgraph_build.cpp
@@ -74,17 +74,14 @@ void D3DXRenderBase::r_dsgraph_insert_dynamic(dxRender_Visual* pVisual, Fvector&
     if (RI.val_bHUD)
     {
         if (sh->flags.bStrictB2F)
-        {
-            mapSorted.emplace_back(std::make_pair(distSQ, _MatrixItemS({ SSA, RI.val_pObject, pVisual, *RI.val_pTransform, sh })));
-        }
+            mapHUDSorted.emplace_back(std::make_pair(distSQ, _MatrixItemS({ SSA, RI.val_pObject, pVisual, *RI.val_pTransform, sh })));
         else
-        {
-            mapHUD.emplace_back(std::make_pair(distSQ, _MatrixItemS({ SSA, RI.val_pObject, pVisual, *RI.val_pTransform, sh })));
+            mapHUD      .emplace_back(std::make_pair(distSQ, _MatrixItemS({ SSA, RI.val_pObject, pVisual, *RI.val_pTransform, sh })));
+
 #if RENDER != R_R1
-            if (sh->flags.bEmissive)
-                mapHUDEmissive.emplace_back(std::make_pair(distSQ, _MatrixItemS({ SSA, RI.val_pObject, pVisual, *RI.val_pTransform, sh_d }))); // sh_d -> L_special
-#endif // RENDER!=R_R1
-        }
+        if (sh->flags.bEmissive)
+            mapHUDEmissive.emplace_back(std::make_pair(distSQ, _MatrixItemS({ SSA, RI.val_pObject, pVisual, *RI.val_pTransform, sh_d }))); // sh_d -> L_special
+#endif
         return;
     }
 

src/Layers/xrRender/D3DXRenderBase.h
@@ -40,6 +40,7 @@ public:
     R_dsgraph::mapHUD_T mapHUD;
     R_dsgraph::mapLOD_T mapLOD;
     R_dsgraph::mapSorted_T mapDistort;
+    R_dsgraph::mapHUD_T    mapHUDSorted;
 
 #if RENDER != R_R1
     R_dsgraph::mapSorted_T mapWmark; // sorted
@@ -142,10 +143,12 @@ public:
         mapHUD.clear();
         mapLOD.clear();
         mapDistort.clear();
+        mapHUDSorted.clear();
 
 #if RENDER != R_R1
         mapWmark.clear();
         mapEmissive.clear();
+        mapHUDEmissive.clear();
 #endif
     }
 

src/Layers/xrRender/r__dsgraph_render.cpp
@@ -386,9 +386,9 @@ void D3DXRenderBase::r_dsgraph_render_hud_ui()
 {
     VERIFY(g_hud && g_hud->RenderActiveItemUIQuery());
 
-    extern ENGINE_API float psHUD_FOV;
-
     PIX_EVENT(r_dsgraph_render_hud_ui);
+
+    extern ENGINE_API float psHUD_FOV;
 
     // Change projection
     Fmatrix Pold = Device.mProject;
@@ -435,6 +435,29 @@ void D3DXRenderBase::r_dsgraph_render_sorted()
     for (auto &i : mapSorted)
         sorted_L1(i);
     mapSorted.clear();
+
+    extern ENGINE_API float psHUD_FOV;
+
+    // Change projection
+    Fmatrix Pold = Device.mProject;
+    Fmatrix FTold = Device.mFullTransform;
+    Device.mProject.build_projection(deg2rad(psHUD_FOV * Device.fFOV /* *Device.fASPECT*/), Device.fASPECT,
+        VIEWPORT_NEAR, g_pGamePersistent->Environment().CurrentEnv->far_plane);
+
+    Device.mFullTransform.mul(Device.mProject, Device.mView);
+    RCache.set_xform_project(Device.mProject);
+
+    // Rendering
+    rmNear();
+    std::sort(mapHUDSorted.begin(), mapHUDSorted.end(), cmp_first_h<R_dsgraph::mapSorted_T::value_type>); // back-to-front
+    for (auto &i : mapHUDSorted)
+        sorted_L1(i);
+    rmNormal();
+
+    // Restore projection
+    Device.mProject = Pold;
+    Device.mFullTransform = FTold;
+    RCache.set_xform_project(Device.mProject);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -467,7 +490,6 @@ void D3DXRenderBase::r_dsgraph_render_emissive()
     for (auto &i : mapHUDEmissive)
         sorted_L1(i);
     mapHUDEmissive.clear();
-
     rmNormal();
 
     // Restore projection

commit 3fcc10d21e385b7cf84c7c5a145dac7f7f06a197

Strict back-to-front HUD objects should not be also added to emissive objects
Fixes possible bug introduced with
0cd8124fd1f1008200433bbf2c09c028a94e1ffd

src/Layers/xrRender/r__dsgraph_build.cpp
@@ -72,9 +72,11 @@ void D3DXRenderBase::r_dsgraph_insert_dynamic(IRenderable* root, dxRender_Visual
     if (root && root->renderable_HUD())
     {
         if (sh->flags.bStrictB2F)
+        {
             mapHUDSorted.insert_anyway(distSQ, _MatrixItemS({ SSA, root, pVisual, xform, sh }));
-        else
-            mapHUD      .insert_anyway(distSQ, _MatrixItemS({ SSA, root, pVisual, xform, sh }));
+            return;
+        }
+        mapHUD.insert_anyway(distSQ, _MatrixItemS({ SSA, root, pVisual, xform, sh }));
 
 #if RENDER != R_R1
         if (sh->flags.bEmissive)