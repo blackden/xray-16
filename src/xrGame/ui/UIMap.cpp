blackden/xray-16

commit 78e9c149af1ee6bf896e6c9f9e1fa83fd9c77221

xrGame/ui/UIMap.h|cpp: initialization functions now will use const CInifile&

src/xrGame/ui/UIMap.cpp
@@ -22,7 +22,7 @@ CUICustomMap::CUICustomMap()
 
 void CUICustomMap::Initialize(shared_str name, LPCSTR sh_name)
 {
-    CInifile* levelIni = nullptr;
+    const CInifile* levelIni{};
     if (name == g_pGameLevel->name())
         levelIni = g_pGameLevel->pLevel;
     else
@@ -65,7 +65,7 @@ void CUICustomMap::Draw()
     UI().PopScissor();
 }
 
-void CUICustomMap::Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
+void CUICustomMap::Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
 {
     m_name = name;
 
@@ -281,7 +281,7 @@ CUIGlobalMap::CUIGlobalMap(CUIMapWnd* pMapWnd)
 
 CUIGlobalMap::~CUIGlobalMap() {}
 void CUIGlobalMap::Initialize() { Init_internal("global_map", *pGameIni, "global_map", "hud" DELIMITER "default"); }
-void CUIGlobalMap::Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
+void CUIGlobalMap::Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
 {
     inherited::Init_internal(name, pLtx, sect_name, sh_name);
     //	Fvector2 size = CUIStatic::GetWndSize();

src/xrGame/ui/UIMap.h
@@ -64,7 +64,7 @@ public:
     void SetPointerDistance(float d) { m_pointer_dist = d; };
     float GetPointerDistance() { return m_pointer_dist; };
 protected:
-    virtual void Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name);
+    virtual void Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name);
     virtual void UpdateSpots(){};
 };
 
@@ -100,7 +100,7 @@ public:
     void Initialize();
 
 protected:
-    virtual void Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name);
+    virtual void Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name);
 };
 
 class CUILevelMap : public CUICustomMap

commit db9fbc46e3d41a2d52cab955816c3064d9770523

Fixed broken map spots in PDA (#693)
Was broken in 78e9c149af1ee6bf896e6c9f9e1fa83fd9c77221

src/xrGame/ui/UIMap.cpp
@@ -438,7 +438,7 @@ void CUILevelMap::Draw()
     inherited::Draw();
 }
 
-void CUILevelMap::Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
+void CUILevelMap::Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
 {
     inherited::Init_internal(name, pLtx, sect_name, sh_name);
     Fvector4 tmp = pGameIni->r_fvector4(MapName(), "global_rect");
@@ -574,7 +574,7 @@ CUIMiniMap::CUIMiniMap()
 
 CUIMiniMap::~CUIMiniMap() {}
 
-void CUIMiniMap::Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
+void CUIMiniMap::Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name)
 {
     inherited::Init_internal(name, pLtx, sect_name, sh_name);
     CUIStatic::SetTextureColor(0x7fffffff);

src/xrGame/ui/UIMap.h
@@ -100,7 +100,7 @@ public:
     void Initialize();
 
 protected:
-    virtual void Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name);
+    void Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name) override;
 };
 
 class CUILevelMap : public CUICustomMap
@@ -127,7 +127,7 @@ public:
 
 protected:
     virtual void UpdateSpots();
-    virtual void Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name);
+    void Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name) override;
 };
 
 // Rounded by default
@@ -146,5 +146,5 @@ public:
 
 protected:
     virtual void UpdateSpots();
-    virtual void Init_internal(const shared_str& name, CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name);
+    void Init_internal(const shared_str& name, const CInifile& pLtx, const shared_str& sect_name, LPCSTR sh_name) override;
 };