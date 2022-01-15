blackden/xray-16

commit 41e8ad6646f17591dfa3ca9f6022aede509e4aa3

Moved CStringTable to xrEngine

src/xrEngine/StringTable/StringTable.cpp
@@ -1,11 +1,17 @@
-#include "StdAfx.h"
-#include "string_table.h"
-#include "xrUICore/XML/xrUIXmlParser.h"
-#include "xrEngine/xr_level_controller.h"
+#include "stdafx.h"
+#include "StringTable.h"
+
+#include "xr_level_controller.h"
+
+#include "xrCore/XML/XMLDocument.hpp"
 
 constexpr pcstr OPENXRAY_XML = "openxray.xml";
 
-CStringTable& StringTable() { return *((CStringTable*)gStringTable); }
+CStringTable& StringTable()
+{
+    static CStringTable string_table;
+    return string_table;
+}
 
 xr_unique_ptr<STRING_TABLE_DATA> CStringTable::pData;
 BOOL CStringTable::m_bWriteErrorsToLog = FALSE;
@@ -146,7 +152,7 @@ xr_token* CStringTable::GetLanguagesToken() const { return languagesToken.data()
 
 void CStringTable::Load(LPCSTR xml_file_full)
 {
-    CUIXml uiXml;
+    XMLDocument uiXml;
     string_path _s;
     strconcat(sizeof(_s), _s, "text" DELIMITER, pData->m_sLanguage.c_str());
 
@@ -199,9 +205,9 @@ void CStringTable::ReparseKeyBindings()
 
 STRING_VALUE CStringTable::ParseLine(LPCSTR str, LPCSTR skey, bool bFirst)
 {
-    constexpr char   ACTION_STR[]       = "$$ACTION_";
-    constexpr char   ACTION_STR_END[]   = "$$";
-    constexpr size_t ACTION_STR_LEN     = std::size(ACTION_STR) - 1;
+    constexpr char   ACTION_STR[] = "$$ACTION_";
+    constexpr char   ACTION_STR_END[] = "$$";
+    constexpr size_t ACTION_STR_LEN = std::size(ACTION_STR) - 1;
     constexpr size_t ACTION_STR_END_LEN = std::size(ACTION_STR_END) - 1;
 
     xr_string res;

src/xrEngine/StringTable/StringTable.h
@@ -3,8 +3,13 @@
 //////////////////////////////////////////////////////////////////////////
 
 #pragma once
-#include "xrEngine/StringTable/IStringTable.h"
+
 #include "xrCommon/xr_map.h"
+#include "xrCore/xrstring.h"
+#include "xrCore/xr_token.h"
+
+using STRING_ID = shared_str;
+using STRING_VALUE = shared_str;
 
 using STRING_TABLE_MAP = xr_map<STRING_ID, STRING_VALUE>;
 
@@ -17,7 +22,7 @@ struct STRING_TABLE_DATA
     STRING_TABLE_MAP m_string_key_binding;
 };
 
-class CStringTable : public IStringTable
+class ENGINE_API CStringTable
 {
 public:
     CStringTable();
@@ -26,13 +31,13 @@ public:
     void Init();
     static void Destroy();
 
-    STRING_VALUE translate(const STRING_ID& str_id) const override;
-    void rescan() override;
+    STRING_VALUE translate(const STRING_ID& str_id) const;
+    void rescan();
 
     void ReloadLanguage();
 
     static BOOL m_bWriteErrorsToLog;
-    void ReparseKeyBindings() override;
+    static void ReparseKeyBindings();
 
     xr_token* GetLanguagesToken() const;
     static u32 LanguageID;
@@ -46,4 +51,4 @@ private:
     static xr_vector<xr_token> languagesToken;
 };
 
-CStringTable& StringTable();
+ENGINE_API CStringTable& StringTable();

src/xrGame/xrGame.cpp
@@ -48,13 +48,11 @@ extern "C"
 #ifdef DEBUG
         g_profiler = xr_new<CProfiler>();
 #endif
-        gStringTable = xr_new<CStringTable>();
         StringTable().Init();
     }
 
     XR_EXPORT void finalize_library()
     {
         CleanupUIStyleToken();
-        xr_delete(gStringTable);
     }
 }

commit 3a0f95836d6f31aed5a7a1ac264f72b79d9b5c5a

Fixed crash on game exit Introduced with 41e8ad6646f17591dfa3ca9f6022aede509e4aa3

src/xrEngine/StringTable/StringTable.cpp
@@ -13,17 +13,11 @@ CStringTable& StringTable()
     return string_table;
 }
 
-xr_unique_ptr<STRING_TABLE_DATA> CStringTable::pData;
+xr_unique_ptr<STRING_TABLE_DATA> CStringTable::pData{};
 BOOL CStringTable::m_bWriteErrorsToLog = FALSE;
 u32 CStringTable::LanguageID = std::numeric_limits<u32>::max();
 xr_vector<xr_token> CStringTable::languagesToken;
 
-CStringTable::CStringTable()
-{
-    pData = nullptr;
-}
-
-CStringTable::~CStringTable() { Destroy(); }
 void CStringTable::Destroy()
 {
     pData.reset(nullptr);

src/xrEngine/StringTable/StringTable.h
@@ -22,12 +22,9 @@ struct STRING_TABLE_DATA
     STRING_TABLE_MAP m_string_key_binding;
 };
 
-class ENGINE_API CStringTable
+class ENGINE_API CStringTable final
 {
 public:
-    CStringTable();
-    virtual ~CStringTable();
-
     void Init();
     static void Destroy();
 

src/xrGame/xrGame.cpp
@@ -54,5 +54,6 @@ extern "C"
     XR_EXPORT void finalize_library()
     {
         CleanupUIStyleToken();
-    }
+        StringTable().Destroy();
+ }
 }