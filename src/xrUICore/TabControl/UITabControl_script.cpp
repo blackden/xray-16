blackden/xray-16

commit 9df13e2d1a4c90996ca06ac39c0255855afa1170

xrUICore/TabControl: support index-based work (#392)
Also enhanced id based functionality

src/xrUICore/TabControl/UITabControl_script.cpp
@@ -14,11 +14,17 @@ SCRIPT_EXPORT(CUITabControl, (CUIWindow),
             .def(constructor<>())
             .def("AddItem", (bool (CUITabControl::*)(CUITabButton*))(&CUITabControl::AddItem), adopt<2>())
             .def("AddItem", (bool (CUITabControl::*)(LPCSTR, LPCSTR, Fvector2, Fvector2)) & CUITabControl::AddItem)
+            .def("RemoveItem", &CUITabControl::RemoveItemByIndex)
+            .def("RemoveItem", &CUITabControl::RemoveItemById_script)
             .def("RemoveAll", &CUITabControl::RemoveAll)
             .def("GetActiveId", &CUITabControl::GetActiveId_script)
+            .def("GetActiveIndex", &CUITabControl::GetActiveIndex)
             .def("GetTabsCount", &CUITabControl::GetTabsCount)
             .def("SetActiveTab", &CUITabControl::SetActiveTab_script)
+            .def("SetActiveTab", &CUITabControl::SetActiveTabByIndex)
+            .def("SetNewActiveTab", &CUITabControl::SetActiveTabByIndex)
             .def("GetButtonById", &CUITabControl::GetButtonById_script)
+            .def("GetButtonByIndex", &CUITabControl::GetButtonByIndex)
     ];
 });
 

src/xrUICore/TabControl/UITabControl.cpp
@@ -2,6 +2,8 @@
 #include "UITabControl.h"
 #include "UITabButton.h"
 
+bool operator==(const CUITabButton* btn, const shared_str& id) { return (btn->m_btn_id == id); }
+
 CUITabControl::CUITabControl()
     : m_cGlobalTextColor(0xFFFFFFFF), m_cActiveTextColor(0xFFFFFFFF), m_cActiveButtonColor(0xFFFFFFFF),
       m_cGlobalButtonColor(0xFFFFFFFF), m_bAcceleratorsEnable(true)
@@ -66,6 +68,31 @@ bool CUITabControl::AddItem(CUITabButton* pButton)
     m_TabsArr.push_back(pButton);
     R_ASSERT(pButton->m_btn_id.size());
     return true;
+}
+
+void CUITabControl::RemoveItemById(const shared_str& id)
+{
+    const auto it = std::find(m_TabsArr.begin(), m_TabsArr.end(), id);
+    const bool tabControlItemFound = it != m_TabsArr.end();
+
+    R_ASSERT(tabControlItemFound);
+    if (tabControlItemFound)
+    {
+        DetachChild(*it);
+        m_TabsArr.erase(it);
+    }
+}
+
+void CUITabControl::RemoveItemByIndex(u32 index)
+{
+    R_ASSERT(m_TabsArr.size() > index);
+
+    // Меняем значение заданного элемента, и последнего элемента.
+    // Так как у нас хранятся указатели операция будет проходить быстро.
+    std::swap(m_TabsArr[index], m_TabsArr.back());
+
+    DetachChild(m_TabsArr.back());
+    m_TabsArr.pop_back();
 }
 
 void CUITabControl::RemoveAll()
@@ -138,6 +165,16 @@ void CUITabControl::OnTabChange(const shared_str& sCur, const shared_str& sPrev)
     GetMessageTarget()->SendMessage(this, TAB_CHANGED, NULL);
 }
 
+int CUITabControl::GetActiveIndex() const
+{
+    for (int i = 0; i < (int)m_TabsArr.size(); ++i)
+    {
+        if (m_TabsArr[i]->m_btn_id == m_sPushedId)
+            return i;
+    }
+    return -1;
+}
+
 void CUITabControl::SetActiveTab(const shared_str& sNewTab)
 {
     if (m_sPushedId == sNewTab)
@@ -147,6 +184,16 @@ void CUITabControl::SetActiveTab(const shared_str& sNewTab)
     OnTabChange(m_sPushedId, m_sPrevPushedId);
 
     m_sPrevPushedId = m_sPushedId;
+}
+
+void CUITabControl::SetActiveTabByIndex(u32 index)
+{
+    CUITabButton* newBtn = GetButtonByIndex(index);
+    CUITabButton* prevBtn = GetButtonById(GetActiveId());
+    if (newBtn == prevBtn)
+        return;
+
+    SetActiveTab(newBtn->m_btn_id);
 }
 
 bool CUITabControl::OnKeyboardAction(int dik, EUIMessages keyboard_action)
@@ -165,15 +212,20 @@ bool CUITabControl::OnKeyboardAction(int dik, EUIMessages keyboard_action)
     return false;
 }
 
-bool operator==(const CUITabButton* btn, const shared_str& id) { return (btn->m_btn_id == id); }
 CUITabButton* CUITabControl::GetButtonById(const shared_str& id)
 {
     TABS_VECTOR::const_iterator it = std::find(m_TabsArr.begin(), m_TabsArr.end(), id);
     if (it != m_TabsArr.end())
         return *it;
-    else
-        return NULL;
+    return nullptr;
 }
+
+CUITabButton* CUITabControl::GetButtonByIndex(u32 index) const
+{
+    R_ASSERT(index < (int)m_TabsArr.size());
+    return m_TabsArr[index];
+}
+
 /*
 const shared_str CUITabControl::GetCommandName(const shared_str& id)
 {
@@ -200,12 +252,6 @@ void CUITabControl::ResetTab()
     }
     m_sPushedId = "";
     m_sPrevPushedId = "";
-}
-
-LPCSTR CUITabControl::GetActiveId_script()
-{
-    LPCSTR res = GetActiveId().c_str();
-    return res;
 }
 
 void CUITabControl::Enable(bool status)

src/xrUICore/TabControl/UITabControl.h
@@ -27,8 +27,11 @@ public:
     virtual void OnStaticFocusLost(CUIWindow* pWnd);
 
     // Добавление кнопки-закладки в список закладок контрола
-    bool AddItem(LPCSTR pItemName, LPCSTR pTexName, Fvector2 pos, Fvector2 size);
+    bool AddItem(cpcstr pItemName, cpcstr pTexName, Fvector2 pos, Fvector2 size);
     bool AddItem(CUITabButton* pButton);
+    void RemoveItemById(const shared_str& id);
+    void RemoveItemById_script(cpcstr id) { RemoveItemById(id); }
+    void RemoveItemByIndex(u32 index);
 
     void RemoveAll();
 
@@ -36,17 +39,27 @@ public:
     virtual void Enable(bool status);
 
     const shared_str& GetActiveId() const { return m_sPushedId; }
-    LPCSTR GetActiveId_script();
+    pcstr GetActiveId_script() const { return GetActiveId().c_str(); }
+    int GetActiveIndex() const;
+
     const shared_str& GetPrevActiveId() { return m_sPrevPushedId; }
+
     void SetActiveTab(const shared_str& sNewTab);
     void SetActiveTab_script(LPCSTR sNewTab) { SetActiveTab(sNewTab); };
+    void SetActiveTabByIndex(u32 index);
+
     const u32 GetTabsCount() const { return m_TabsArr.size(); }
+
     // Режим клавилатурных акселераторов (вкл/выкл)
     IC bool GetAcceleratorsMode() const { return m_bAcceleratorsEnable; }
     void SetAcceleratorsMode(bool bEnable) { m_bAcceleratorsEnable = bEnable; }
+
     TABS_VECTOR* GetButtonsVector() { return &m_TabsArr; }
+
     CUITabButton* GetButtonById(const shared_str& id);
-    CUITabButton* GetButtonById_script(LPCSTR s) { return GetButtonById(s); }
+    CUITabButton* GetButtonById_script(cpcstr s) { return GetButtonById(s); }
+    CUITabButton* GetButtonByIndex(u32 index) const;
+
     void ResetTab();
 
 protected:

commit 7bf02cb788fa9d77a95794d6955442c31ad08e39

Fixed CUITabControl::SetActiveTab script function being overriden incorrectly (fixes #722)
Bug was introduced in 9df13e2d1a4c90996ca06ac39c0255855afa1170.
We just don't need this excessive overload, SOC scripts use
SetNewActiveTab only,

src/xrUICore/TabControl/UITabControl_script.cpp
@@ -21,7 +21,6 @@ SCRIPT_EXPORT(CUITabControl, (CUIWindow),
             .def("GetActiveIndex", &CUITabControl::GetActiveIndex)
             .def("GetTabsCount", &CUITabControl::GetTabsCount)
             .def("SetActiveTab", &CUITabControl::SetActiveTab_script)
-            .def("SetActiveTab", &CUITabControl::SetActiveTabByIndex)
             .def("SetNewActiveTab", &CUITabControl::SetActiveTabByIndex)
             .def("GetButtonById", &CUITabControl::GetButtonById_script)
             .def("GetButtonByIndex", &CUITabControl::GetButtonByIndex)