blackden/xray-16

commit 781d479a4815af5c749b8ab4a861b6ac2d5ab0da

Refactored CEnvironment and corresponding weather classes Better code organization, potentially faster Looks more like SOC code in this case. In CS/COP code was refactored to support weather editor based on C++/CLI, but refactoring introduced some messiness in the code. Since we are using different approach with the new weather editor, we can make code better again.

src/xrEngine/Environment_misc.cpp
@@ -285,10 +285,10 @@ CEnvDescriptor::CEnvDescriptor(shared_str const& identifier) : m_identifier(iden
     m_fSunShaftsIntensity = 0;
     m_fWaterIntensity = 1;
 
-    m_fTreeAmplitudeIntensity = 0.01;
+    m_fTreeAmplitudeIntensity = 0.01f;
 
-    lens_flare_id = "";
-    tb_id = "";
+    lens_flare = nullptr;
+    thunderbolt = nullptr;
 
     env_ambient = nullptr;
 }
@@ -301,7 +301,15 @@ CEnvDescriptor::CEnvDescriptor(shared_str const& identifier) : m_identifier(iden
 
 void CEnvDescriptor::load(CEnvironment& environment, const CInifile& config, pcstr section /*= nullptr*/)
 {
-    const bool oldStyle = section;
+    const bool old_style               = section;
+
+    cpcstr ambient_name                = old_style ? "env_ambient"   : "ambient";
+    cpcstr ambient_color_name          = old_style ? "ambient"       : "ambient_color";
+    cpcstr sun_name                    = old_style ? "flares"        : "sun";
+    cpcstr thunderbolt_collection_name = old_style ? "thunderbolt"   : "thunderbolt_collection";
+    cpcstr thunderbolt_duration_name   = old_style ? "bolt_duration" : "thunderbolt_duration";
+    cpcstr thunderbolt_period_name     = old_style ? "bolt_period"   : "thunderbolt_period";
+
     cpcstr identifier = section ? section : m_identifier.c_str();
 
     Ivector3 tm = {0, 0, 0};
@@ -328,15 +336,9 @@ void CEnvDescriptor::load(CEnvironment& environment, const CInifile& config, pcs
     clouds_color.mul(.5f * multiplier);
     clouds_color.w = save;
 
-    if (oldStyle)
-    {
-        sky_color = config.r_fvector3(identifier, "sky_color");
+    sky_color = config.r_fvector3(identifier, "sky_color");
+    if (old_style)
         sky_color.mul(0.5f);
-    }
-    else
-    {
-        sky_color = config.r_fvector3(identifier, "sky_color");
-    }
 
     if (config.line_exist(identifier, "sky_rotation"))
         sky_rotation = deg2rad(config.r_float(identifier, "sky_rotation"));
@@ -362,18 +364,11 @@ void CEnvDescriptor::load(CEnvironment& environment, const CInifile& config, pcs
 
     sun_color = config.r_fvector3(identifier, "sun_color");
 
-    if (oldStyle)
+    ambient = config.r_fvector3(identifier, ambient_color_name);
+    if (config.line_exist(identifier, ambient_name))
     {
-        ambient = pSettings->r_fvector3(identifier, "ambient");
-
-        if (config.line_exist(identifier, "env_ambient"))
-            env_ambient = environment.AppendEnvAmb(config.r_string(identifier, "env_ambient"), pSettings);
-    }
-    else
-    {
-        ambient = config.r_fvector3(identifier, "ambient_color");
-        if (config.line_exist(identifier, "ambient"))
-            env_ambient = environment.AppendEnvAmb(config.r_string(identifier, "ambient"));
+        CInifile const* pIni = &config == pSettings ? pSettings : nullptr;
+        env_ambient = environment.AppendEnvAmb(config.r_string(identifier, ambient_name), pIni);
     }
 
     Fvector2 sunVec{};
@@ -396,27 +391,13 @@ void CEnvDescriptor::load(CEnvironment& environment, const CInifile& config, pcs
     clamp(degrees, 0.0f, 360.0f);
     sun_azimuth = deg2rad(degrees);
 
-    if (oldStyle)
-    {
-        lens_flare_id = environment.eff_LensFlare->AppendDef(environment, pSettings,
-            config.r_string(section, "flares"));
-        tb_id = environment.eff_Thunderbolt->AppendDef(environment, pSettings,
-            pSettings, config.r_string(section, "thunderbolt"));
-    }
-    else
-    {
-        lens_flare_id = environment.eff_LensFlare->AppendDef(
-            environment, environment.m_suns_config, config.r_string(identifier, "sun"));
-        tb_id = environment.eff_Thunderbolt->AppendDef(environment, environment.m_thunderbolt_collections_config,
-            environment.m_thunderbolts_config, config.r_string(identifier, "thunderbolt_collection"));
-    }
+    lens_flare = environment.eff_LensFlare->AppendDef(config.r_string(identifier, sun_name));
+    thunderbolt = environment.eff_Thunderbolt->AppendDef(config.r_string(identifier, thunderbolt_collection_name));
 
-    if (tb_id.size())
+    if (thunderbolt)
     {
-        config.read_if_exists(bolt_period, identifier,
-            "thunderbolt_period", "bolt_period", true);
-        config.read_if_exists(bolt_duration, identifier,
-            "thunderbolt_duration", "bolt_duration", true);
+        bolt_period = config.r_float(m_identifier.c_str(), thunderbolt_period_name);
+        bolt_duration = config.r_float(m_identifier.c_str(), thunderbolt_duration_name);
     }
 
     m_fSunShaftsIntensity = config.read_if_exists<float>(identifier, "sun_shafts_intensity", 0.0);
@@ -475,7 +456,7 @@ void CEnvDescriptor::save(CInifile& config, pcstr section /*= nullptr*/) const
     config.w_float    (identifier, "sky_rotation",              rad2deg(sky_rotation));
     config.w_string   (identifier, "sky_texture",               sky_texture_name.c_str());
 
-    config.w_string   (identifier, sun_name,                    lens_flare_id.c_str());
+    config.w_string   (identifier, sun_name,                    lens_flare->section.c_str());
     config.w_fvector3 (identifier, "sun_color",                 sun_color);
     if (old_style)
     {
@@ -491,7 +472,7 @@ void CEnvDescriptor::save(CInifile& config, pcstr section /*= nullptr*/) const
     }
     config.w_float    (identifier, "sun_shafts_intensity",      m_fSunShaftsIntensity);
 
-    config.w_string   (identifier, thunderbolt_collection_name, tb_id.c_str());
+    config.w_string   (identifier, thunderbolt_collection_name, thunderbolt ? thunderbolt->section.c_str() : "");
     config.w_float    (identifier, thunderbolt_duration_name,   bolt_duration);
     config.w_float    (identifier, thunderbolt_period_name,     bolt_period);
 
@@ -619,8 +600,8 @@ void CEnvDescriptorMixer::lerp(CEnvironment& parent, CEnvDescriptor& A, CEnvDesc
     }
     VERIFY2(sun_dir.y < 0, "Invalid sun direction settings while lerp");
 
-    lens_flare_id = f < 0.5f ? A.lens_flare_id : B.lens_flare_id;
-    tb_id = f < 0.5f ? A.tb_id : B.tb_id;
+    lens_flare = f < 0.5f ? A.lens_flare : B.lens_flare;
+    thunderbolt = f < 0.5f ? A.thunderbolt : B.thunderbolt;
     env_ambient = Random.randF() < 1.f - f ? A.env_ambient : B.env_ambient;
 
     if (soc_style)

src/xrEngine/thunderbolt.cpp
@@ -48,7 +48,7 @@ void SThunderboltDesc::create_center_gradient(const CInifile& pIni, shared_str c
     m_GradientCenter->m_pFlare->CreateShader(*m_GradientCenter->shader, *m_GradientCenter->texture);
 }
 
-void SThunderboltDesc::load(const CInifile& pIni, shared_str const& sect)
+SThunderboltDesc::SThunderboltDesc(const CInifile& pIni, shared_str const& sect)
 {
     create_top_gradient(pIni, sect);
     create_center_gradient(pIni, sect);
@@ -74,22 +74,23 @@ void SThunderboltDesc::load(const CInifile& pIni, shared_str const& sect)
 //----------------------------------------------------------------------------------------------
 // collection
 //----------------------------------------------------------------------------------------------
-void SThunderboltCollection::load(CInifile const* pIni, CInifile const* thunderbolts, pcstr sect)
+SThunderboltCollection::SThunderboltCollection(shared_str sect, CInifile const* pIni, CInifile const* thunderbolts)
 {
     section = sect;
-    int tb_count = pIni->line_count(sect);
+    const int tb_count = pIni->line_count(sect);
+    palette.reserve(tb_count);
     for (int tb_idx = 0; tb_idx < tb_count; tb_idx++)
     {
         pcstr N, V;
         if (pIni->r_line(sect, tb_idx, &N, &V))
-            palette.push_back(g_pGamePersistent->Environment().thunderbolt_description(*thunderbolts, N));
+            palette.emplace_back(xr_new<SThunderboltDesc>(*thunderbolts, N));
     }
 }
+
 SThunderboltCollection::~SThunderboltCollection()
 {
-    for (auto d_it = palette.begin(); d_it != palette.end(); ++d_it)
-        xr_delete(*d_it);
-
+    for (auto& desc : palette)
+        xr_delete(desc);
     palette.clear();
 }
 
@@ -105,15 +106,20 @@ CEffect_Thunderbolt::CEffect_Thunderbolt()
     bEnabled = false;
 
     string_path filePath;
-    pcstr section;
-    CInifile const* config;
-
-    if (FS.update_path(filePath, "$game_config$", "environment\\environment.ltx"))
+    const auto load_config = [&filePath](pcstr path) -> CInifile*
     {
-        section = "environment";
-        config = xr_new<CInifile>(filePath, true, true, false);
-    }
-    else
+        if (FS.update_path(filePath, "$game_config$", path, false))
+            return xr_new<CInifile>(filePath, true, true, false);
+        return nullptr;
+    };
+
+    m_thunderbolt_collections_config = load_config("environment\\thunderbolt_collections.ltx");
+    m_thunderbolts_config = load_config("environment\\thunderbolts.ltx");
+
+    pcstr section = "environment";
+    CInifile const* config = load_config("environment\\environment.ltx");
+
+    if (!config)
     {
         section = "thunderbolt_common";
         config = pSettings;
@@ -142,24 +148,30 @@ CEffect_Thunderbolt::CEffect_Thunderbolt()
 
 CEffect_Thunderbolt::~CEffect_Thunderbolt()
 {
-    for (auto d_it = collection.begin(); d_it != collection.end(); ++d_it)
-        xr_delete(*d_it);
+    collections.clear();
 
-    collection.clear();
+    CInifile::Destroy(m_thunderbolt_collections_config);
+    m_thunderbolt_collections_config = nullptr;
+
+    CInifile::Destroy(m_thunderbolts_config);
+    m_thunderbolts_config = nullptr;
 }
 
-shared_str CEffect_Thunderbolt::AppendDef(
-    CEnvironment& environment, CInifile const* pIni, CInifile const* thunderbolts, pcstr sect)
+SThunderboltCollection* CEffect_Thunderbolt::AppendDef(shared_str sect)
 {
     if (!sect || (0 == sect[0]))
-        return "";
+        return nullptr;
 
-    for (const auto item : collection)
+    for (const auto& item : collections)
         if (item->section == sect)
-            return item->section;
+            return item;
 
-    collection.push_back(environment.thunderbolt_collection(pIni, thunderbolts, sect));
-    return collection.back()->section;
+    auto* result = xr_new<SThunderboltCollection>(sect,
+        m_thunderbolt_collections_config ? m_thunderbolt_collections_config : pSettings,
+        m_thunderbolts_config ? m_thunderbolts_config : pSettings
+    );
+
+    return collections.emplace_back(result);
 }
 
 bool CEffect_Thunderbolt::RayPick(const Fvector& s, const Fvector& d, float& range)
@@ -191,26 +203,28 @@ bool CEffect_Thunderbolt::RayPick(const Fvector& s, const Fvector& d, float& ran
 #endif
     return bRes;
 }
-#define FAR_DIST g_pGamePersistent->Environment().CurrentEnv->far_plane
 
-void CEffect_Thunderbolt::Bolt(shared_str id, float period, float lt)
+void CEffect_Thunderbolt::Bolt(const CEnvDescriptorMixer& currentEnv)
 {
-    VERIFY(id.size());
+    VERIFY(currentEnv.thunderbolt);
     state = stWorking;
+    const float lt = currentEnv.bolt_duration;
     life_time = lt + Random.randF(-lt * 0.5f, lt * 0.5f);
     current_time = 0.f;
 
-    current = g_pGamePersistent->Environment().thunderbolt_collection(collection, id)->GetRandomDesc();
+    current = currentEnv.thunderbolt->GetRandomDesc();
     VERIFY(current);
+
+    float sun_h, sun_p;
+    currentEnv.sun_dir.getHP(sun_h, sun_p);
+    const auto far_dist = currentEnv.far_plane;
+    const float period = currentEnv.bolt_period;
 
     Fmatrix XF, S;
     Fvector pos, dev;
-    float sun_h, sun_p;
-    CEnvironment& environment = g_pGamePersistent->Environment();
-    environment.CurrentEnv->sun_dir.getHP(sun_h, sun_p);
     float alt = Random.randF(p_var_alt.x, p_var_alt.y);
     float lng = Random.randF(sun_h - p_var_long + PI, sun_h + p_var_long + PI);
-    float dist = Random.randF(FAR_DIST * p_min_dist, FAR_DIST * .95f);
+    float dist = Random.randF(far_dist * p_min_dist, far_dist * .95f);
     current_direction.setHP(lng, alt);
     pos.mad(Device.vCameraPosition, current_direction, dist);
     dev.x = Random.randF(-p_tilt, p_tilt);
@@ -218,9 +232,9 @@ void CEffect_Thunderbolt::Bolt(shared_str id, float period, float lt)
     dev.z = Random.randF(-p_tilt, p_tilt);
     XF.setXYZi(dev);
 
-    Fvector light_dir = {0.f, -1.f, 0.f};
+    Fvector light_dir = { 0.f, -1.f, 0.f };
     XF.transform_dir(light_dir);
-    lightning_size = FAR_DIST * 2.f;
+    lightning_size = far_dist * 2.f;
     RayPick(pos, light_dir, lightning_size);
 
     lightning_center.mad(pos, light_dir, lightning_size * 0.5f);
@@ -244,18 +258,19 @@ void CEffect_Thunderbolt::Bolt(shared_str id, float period, float lt)
     current_direction.invert(); // for env-sun
 }
 
-void CEffect_Thunderbolt::OnFrame(shared_str id, float period, float duration)
+void CEffect_Thunderbolt::OnFrame(CEnvDescriptorMixer& currentEnv)
 {
-    bool enabled = !!(id.size());
+    const bool enabled = currentEnv.thunderbolt;
     if (bEnabled != enabled)
     {
         bEnabled = enabled;
+        const float period = currentEnv.bolt_period;
         next_lightning_time = Device.fTimeGlobal + period + Random.randF(-period * 0.5f, period * 0.5f);
     }
     else if (bEnabled && (Device.fTimeGlobal > next_lightning_time))
     {
-        if (state == stIdle && !!(id.size()))
-            Bolt(id, period, duration);
+        if (state == stIdle && currentEnv.thunderbolt)
+            Bolt(currentEnv);
     }
     if (state == stWorking)
     {
@@ -271,22 +286,20 @@ void CEffect_Thunderbolt::OnFrame(shared_str id, float period, float duration)
         lightning_phase = 1.5f * (current_time / life_time);
         clamp(lightning_phase, 0.f, 1.f);
 
-        const auto& environment = g_pGamePersistent->Environment();
-
-        Fvector& sky_color = environment.CurrentEnv->sky_color;
+        Fvector& sky_color = currentEnv.sky_color;
         sky_color.mad(fClr, p_sky_color);
         clamp(sky_color.x, 0.f, 1.f);
         clamp(sky_color.y, 0.f, 1.f);
         clamp(sky_color.z, 0.f, 1.f);
 
-        environment.CurrentEnv->sun_color.mad(fClr, p_sun_color);
-        environment.CurrentEnv->fog_color.mad(fClr, p_fog_color);
+        currentEnv.sun_color.mad(fClr, p_sun_color);
+        currentEnv.fog_color.mad(fClr, p_fog_color);
 
         if (GEnv.Render->GenerationIsR2OrHigher())
         {
             R_ASSERT(_valid(current_direction));
-            environment.CurrentEnv->sun_dir = current_direction;
-            VERIFY2(environment.CurrentEnv->sun_dir.y < 0,
+            currentEnv.sun_dir = current_direction;
+            VERIFY2(currentEnv.sun_dir.y < 0,
                 "Invalid sun direction settings while CEffect_Thunderbolt");
         }
     }

src/xrEngine/xr_efflensflare.cpp
@@ -76,7 +76,7 @@ FlareDescriptorFields SunFields =
     "sun", "sun_shader", "sun_texture", "sun_radius", "sun_ignore_color"
 };
 
-void CLensFlareDescriptor::load(CInifile const* pIni, pcstr sect)
+void CLensFlareDescriptor::load(CInifile const* pIni, shared_str sect)
 {
     section = sect;
 
@@ -183,6 +183,10 @@ CLensFlare::CLensFlare()
     }
 #endif
 
+    string_path filePath;
+    if (FS.update_path(filePath, "$game_config$", "environment\\suns.ltx", false))
+        m_suns_config = xr_new<CInifile>(filePath, true, true, false);
+
     OnDeviceCreate();
 }
 
@@ -190,6 +194,8 @@ CLensFlare::~CLensFlare()
 {
     OnDeviceDestroy();
     delete_data(m_Palette);
+    CInifile::Destroy(m_suns_config);
+    m_suns_config = nullptr;
 }
 
 #ifndef _EDITOR
@@ -273,7 +279,7 @@ static pcstr state_to_string(const CLensFlare::LFState& state)
 static Fvector2 RayDeltas[CLensFlare::MAX_RAYS] = {
     {0, 0}, {1, 0}, {-1, 0}, {0, -1}, {0, 1},
 };
-void CLensFlare::OnFrame(shared_str id)
+void CLensFlare::OnFrame(const CEnvDescriptorMixer& currentEnv, float time_factor)
 {
     if (dwFrame == Device.dwFrame)
         return;
@@ -283,51 +289,48 @@ void CLensFlare::OnFrame(shared_str id)
 #endif
     dwFrame = Device.dwFrame;
 
-    R_ASSERT(_valid(g_pGamePersistent->Environment().CurrentEnv->sun_dir));
-    vSunDir.mul(g_pGamePersistent->Environment().CurrentEnv->sun_dir, -1);
+    R_ASSERT(_valid(currentEnv.sun_dir));
+    vSunDir.mul(currentEnv.sun_dir, -1);
     R_ASSERT(_valid(vSunDir));
 
     // color
-    float tf = g_pGamePersistent->Environment().fTimeFactor;
-    Fvector& c = g_pGamePersistent->Environment().CurrentEnv->sun_color;
-    LightColor.set(c.x, c.y, c.z, 1.f);
+    {
+        const auto& [x, y, z] = currentEnv.sun_color;
+        LightColor.set(x, y, z, 1.f);
+    }
 
-    CInifile const* pIni = g_pGamePersistent->Environment().m_suns_config;
-    if (!pIni)
-        pIni = pSettings;
-
-    CLensFlareDescriptor* desc = id.size() ? g_pGamePersistent->Environment().add_flare(m_Palette, id, pIni) : nullptr;
+    auto flare = currentEnv.lens_flare;
 
     // LFState previous_state = m_State;
     switch (m_State)
     {
     case lfsNone:
         m_State = lfsShow;
-        m_Current = desc;
+        m_Current = flare;
         break;
     case lfsIdle:
-        if (desc != m_Current)
+        if (flare != m_Current)
             m_State = lfsHide;
         break;
     case lfsShow:
-        m_StateBlend = m_Current ? (m_StateBlend + m_Current->m_StateBlendUpSpeed * Device.fTimeDelta * tf) : 1.f + EPS;
+        m_StateBlend = m_Current ? (m_StateBlend + m_Current->m_StateBlendUpSpeed * Device.fTimeDelta * time_factor) : 1.f + EPS;
         if (m_StateBlend >= 1.f)
             m_State = lfsIdle;
         break;
     case lfsHide:
-        m_StateBlend = m_Current ? (m_StateBlend - m_Current->m_StateBlendDnSpeed * Device.fTimeDelta * tf) : 0.f - EPS;
+        m_StateBlend = m_Current ? (m_StateBlend - m_Current->m_StateBlendDnSpeed * Device.fTimeDelta * time_factor) : 0.f - EPS;
         if (m_StateBlend <= 0.f)
         {
             m_State = lfsShow;
-            m_Current = desc;
-            m_StateBlend = m_Current ? m_Current->m_StateBlendUpSpeed * Device.fTimeDelta * tf : 0;
+            m_Current = flare;
+            m_StateBlend = m_Current ? m_Current->m_StateBlendUpSpeed * Device.fTimeDelta * time_factor : 0;
         }
         break;
     }
     // Msg ("%6d : [%s] -> [%s]", Device.dwFrame, state_to_string(previous_state), state_to_string(m_State));
     clamp(m_StateBlend, 0.f, 1.f);
 
-    if ((m_Current == 0) || (LightColor.magnitude_rgb() == 0.f))
+    if (!m_Current || LightColor.magnitude_rgb() == 0.f)
     {
         bRender = false;
         return;
@@ -536,13 +539,18 @@ void CLensFlare::Render(bool bSun, bool bFlares, bool bGradient)
     m_pRender->Render(*this, bSun, bFlares, bGradient);
 }
 
-shared_str CLensFlare::AppendDef(CEnvironment& environment, CInifile const* pIni, pcstr sect)
+CLensFlareDescriptor* CLensFlare::AppendDef(shared_str sect)
 {
     if (!sect || (0 == sect[0]))
-        return "";
+        return nullptr;
 
-    environment.add_flare(m_Palette, sect, pIni);
-    return sect;
+    for (CLensFlareDescriptor& flare : m_Palette)
+        if (flare.section == sect)
+            return &flare;
+
+    CLensFlareDescriptor& descriptor = m_Palette.emplace_back();
+    descriptor.load(m_suns_config ? m_suns_config : pSettings, sect);
+    return &descriptor;
 }
 
 void CLensFlare::OnDeviceCreate()
@@ -551,15 +559,15 @@ void CLensFlare::OnDeviceCreate()
     m_pRender->OnDeviceCreate();
 
     // palette
-    for (const auto& descr : m_Palette)
-        descr->OnDeviceCreate();
+    for (auto& descr : m_Palette)
+        descr.OnDeviceCreate();
 }
 
 void CLensFlare::OnDeviceDestroy()
 {
     // palette
-    for (const auto& descr : m_Palette)
-        descr->OnDeviceDestroy();
+    for (auto& descr : m_Palette)
+        descr.OnDeviceDestroy();
 
     // VS
     m_pRender->OnDeviceDestroy();

src/xrEngine/Environment.cpp
@@ -96,9 +96,6 @@ CEnvironment::CEnvironment() : CurrentEnv(0), m_ambients_config(0)
     m_ambients_config                = load_config("environment\\ambients.ltx");
     m_sound_channels_config          = load_config("environment\\sound_channels.ltx");
     m_effects_config                 = load_config("environment\\effects.ltx");
-    m_suns_config                    = load_config("environment\\suns.ltx");
-    m_thunderbolt_collections_config = load_config("environment\\thunderbolt_collections.ltx");
-    m_thunderbolts_config            = load_config("environment\\thunderbolts.ltx");
 }
 
 CEnvironment::~CEnvironment()
@@ -114,15 +111,6 @@ CEnvironment::~CEnvironment()
 
     CInifile::Destroy(m_effects_config);
     m_effects_config = nullptr;
-
-    CInifile::Destroy(m_suns_config);
-    m_suns_config = nullptr;
-
-    CInifile::Destroy(m_thunderbolt_collections_config);
-    m_thunderbolt_collections_config = nullptr;
-
-    CInifile::Destroy(m_thunderbolts_config);
-    m_thunderbolts_config = nullptr;
 
     destroy_mixer();
 }
@@ -467,8 +455,8 @@ void CEnvironment::OnFrame()
     PerlinNoise1D->SetFrequency(wind_gust_factor * MAX_NOISE_FREQ);
     wind_strength_factor = clampr(PerlinNoise1D->GetContinious(Device.fTimeGlobal) + 0.5f, 0.f, 1.f);
 
-    eff_LensFlare->OnFrame(CurrentEnv->lens_flare_id);
-    eff_Thunderbolt->OnFrame(CurrentEnv->tb_id, CurrentEnv->bolt_period, CurrentEnv->bolt_duration);
+    eff_LensFlare->OnFrame(*CurrentEnv, fTimeFactor);
+    eff_Thunderbolt->OnFrame(*CurrentEnv);
     eff_Rain->OnFrame();
 }
 
@@ -479,40 +467,3 @@ void CEnvironment::create_mixer()
 }
 
 void CEnvironment::destroy_mixer() { xr_delete(CurrentEnv); }
-SThunderboltDesc* CEnvironment::thunderbolt_description(const CInifile& config, shared_str const& section)
-{
-    SThunderboltDesc* result = xr_new<SThunderboltDesc>();
-    result->load(config, section);
-    return (result);
-}
-
-SThunderboltCollection* CEnvironment::thunderbolt_collection(CInifile const* pIni, CInifile const* thunderbolts, pcstr section)
-{
-    SThunderboltCollection* result = xr_new<SThunderboltCollection>();
-    result->load(pIni, thunderbolts, section);
-    return (result);
-}
-
-SThunderboltCollection* CEnvironment::thunderbolt_collection(
-    xr_vector<SThunderboltCollection*>& collection, shared_str const& id)
-{
-    for (auto& it : collection)
-        if (it->section == id)
-            return it;
-
-    NODEFAULT;
-    return nullptr;
-}
-
-CLensFlareDescriptor* CEnvironment::add_flare(
-    xr_vector<CLensFlareDescriptor*>& collection, shared_str const& id, CInifile const* pIni)
-{
-    for (const auto& it: collection)
-        if (it->section == id)
-            return it;
-
-    CLensFlareDescriptor* result = xr_new<CLensFlareDescriptor>();
-    result->load(pIni, id.c_str());
-    collection.push_back(result);
-    return result;
-}

src/xrEngine/Environment.h
@@ -173,8 +173,8 @@ public:
 
     float m_fTreeAmplitudeIntensity;
 
-    shared_str lens_flare_id;
-    shared_str tb_id;
+    CLensFlareDescriptor* lens_flare;
+    SThunderboltCollection* thunderbolt;
 
     CEnvAmbient* env_ambient;
 
@@ -354,9 +354,6 @@ public:
     CInifile* m_ambients_config;
     CInifile* m_sound_channels_config;
     CInifile* m_effects_config;
-    CInifile* m_suns_config;
-    CInifile* m_thunderbolt_collections_config;
-    CInifile* m_thunderbolts_config;
 
 protected:
     virtual CEnvDescriptor* create_descriptor(shared_str const& identifier, CInifile const* config, pcstr section = nullptr);
@@ -369,15 +366,6 @@ protected:
 
     void save_weathers(CInifile* environment_config = nullptr) const;
     void save_weather_effects(CInifile* environment_config = nullptr) const;
-
-public:
-    virtual SThunderboltDesc* thunderbolt_description(const CInifile& config, shared_str const& section);
-    virtual SThunderboltCollection* thunderbolt_collection(
-        CInifile const* pIni, CInifile const* thunderbolts, pcstr section);
-    virtual SThunderboltCollection* thunderbolt_collection(
-        xr_vector<SThunderboltCollection*>& collection, shared_str const& id);
-    virtual CLensFlareDescriptor* add_flare(
-        xr_vector<CLensFlareDescriptor*>& collection, shared_str const& id, CInifile const* pIni);
 };
 
 ENGINE_API extern Flags32 psEnvFlags;

src/xrEngine/editor_weather.cpp
@@ -7,6 +7,7 @@
 #include "thunderbolt.h"
 #include "IGame_Level.h"
 #include "IGame_Persistent.h"
+#include "xr_efflensflare.h"
 
 namespace xray::editor
 {
@@ -122,18 +123,18 @@ void display_property(CEnvDescriptor& descriptor)
     ImGui::PushID(descriptor.m_identifier.c_str());
     if (ImGui::CollapsingHeader("sun##category", ImGuiTreeNodeFlags_DefaultOpen))
     {
-        if (ImGui::BeginCombo("sun##lensflareid", descriptor.lens_flare_id.c_str()))
+        if (ImGui::BeginCombo("sun##lensflareid", descriptor.lens_flare ? descriptor.lens_flare->section.c_str() : ""))
         {
             if (ImGui::Selectable("<edit>", false))
             {
                 window_suns = true;
             }
-            if (ImGui::Selectable("##", descriptor.lens_flare_id.empty()))
-                descriptor.lens_flare_id = "";
-            for (const auto& section : env.m_suns_config->sections())
+            if (ImGui::Selectable("##", !descriptor.lens_flare))
+                descriptor.lens_flare = nullptr;
+            for (auto& desc : env.eff_LensFlare->GetDescriptors())
             {
-                if (ImGui::Selectable(section->Name.c_str(), section->Name == descriptor.lens_flare_id))
-                    descriptor.lens_flare_id = section->Name;
+                if (ImGui::Selectable(desc.section.c_str(), &desc == descriptor.lens_flare))
+                    descriptor.lens_flare = &desc;
             }
             ImGui::EndCombo();
         }
@@ -251,18 +252,18 @@ void display_property(CEnvDescriptor& descriptor)
     }
     if (ImGui::CollapsingHeader("thunderbolts##category", ImGuiTreeNodeFlags_DefaultOpen))
     {
-        if (ImGui::BeginCombo("thunderbolts", descriptor.tb_id.c_str()))
+        if (ImGui::BeginCombo("thunderbolts", descriptor.thunderbolt ? descriptor.thunderbolt->section.c_str() : ""))
         {
             if (ImGui::Selectable("<edit>", false))
             {
                 window_thunderbolts = true;
             }
-            if (ImGui::Selectable("##", descriptor.tb_id.empty()))
-                descriptor.tb_id = "";
-            for (const auto& section : env.m_thunderbolt_collections_config->sections())
+            if (ImGui::Selectable("##", !descriptor.thunderbolt))
+                descriptor.thunderbolt = nullptr;
+            for (const auto& collection : env.eff_Thunderbolt->GetCollections())
             {
-                if (ImGui::Selectable(section->Name.c_str(), section->Name == descriptor.tb_id))
-                    descriptor.tb_id = section->Name;
+                if (ImGui::Selectable(collection->section.c_str(), collection == descriptor.thunderbolt))
+                    descriptor.thunderbolt = collection;
             }
             ImGui::EndCombo();
         }
@@ -272,7 +273,6 @@ void display_property(CEnvDescriptor& descriptor)
 
         ImGui::DragFloat("duration", &descriptor.bolt_duration);
         ImGui::DragFloat("period", &descriptor.bolt_period);
-
     }
     if (ImGui::CollapsingHeader("wind##category", ImGuiTreeNodeFlags_DefaultOpen))
     {

commit e7685bd36614a49b941eab2f9c014c0d1bda0843

Fix environment configs loading in SOC mode Was broken in 781d479a4815af5c749b8ab4a861b6ac2d5ab0da

src/xrEngine/Environment_misc.cpp
@@ -396,8 +396,8 @@ void CEnvDescriptor::load(CEnvironment& environment, const CInifile& config, pcs
 
     if (thunderbolt)
     {
-        bolt_period = config.r_float(m_identifier.c_str(), thunderbolt_period_name);
-        bolt_duration = config.r_float(m_identifier.c_str(), thunderbolt_duration_name);
+        bolt_period = config.r_float(identifier, thunderbolt_period_name);
+        bolt_duration = config.r_float(identifier, thunderbolt_duration_name);
     }
 
     m_fSunShaftsIntensity = config.read_if_exists<float>(identifier, "sun_shafts_intensity", 0.0);

src/xrEngine/thunderbolt.cpp
@@ -89,10 +89,10 @@ CEffect_Thunderbolt::CEffect_Thunderbolt()
     next_lightning_time = 0.f;
     bEnabled = false;
 
-    string_path filePath;
-    const auto load_config = [&filePath](pcstr path) -> CInifile*
+    const auto load_config = [](pcstr path) -> CInifile*
     {
-        if (FS.update_path(filePath, "$game_config$", path, false))
+        string_path filePath;
+        if (FS.exist(filePath, "$game_config$", path))
             return xr_new<CInifile>(filePath, true, true, false);
         return nullptr;
     };

src/xrEngine/xr_efflensflare.cpp
@@ -184,7 +184,7 @@ CLensFlare::CLensFlare()
 #endif
 
     string_path filePath;
-    if (FS.update_path(filePath, "$game_config$", "environment\\suns.ltx", false))
+    if (FS.exist(filePath, "$game_config$", "environment\\suns.ltx"))
         m_suns_config = xr_new<CInifile>(filePath, true, true, false);
 
     OnDeviceCreate();