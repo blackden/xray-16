blackden/xray-16

commit 1e34505457cc34ec397f1bc0e47bc81483fa7b87

xrGame/visual_memory_manager.cpp: load vision_free_section or default section (#382)

src/xrGame/visual_memory_manager.cpp
@@ -149,8 +149,8 @@ void CVisualMemoryManager::reload(LPCSTR section)
     }
     else if (m_object)
     {
-        m_free.Load(pSettings->r_string(section, "vision_free_section"), !!m_client);
-        m_danger.Load(pSettings->r_string(section, "vision_danger_section"), !!m_client);
+        m_free.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
+        m_danger.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
     }
     else
     {

commit 6356c6c719f062fabe9e8bc725559e9abd01e648

xrGame/visual_memory_manager.cpp: load vision_danger_section correctly
This fixes bug introduced in 1e34505457cc34ec397f1bc0e47bc81483fa7b87

src/xrGame/visual_memory_manager.cpp
@@ -152,7 +152,7 @@ void CVisualMemoryManager::reload(LPCSTR section)
     else if (m_object)
     {
         m_free.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
-        m_danger.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_free_section", section), !!m_client);
+        m_danger.Load(READ_IF_EXISTS(pSettings, r_string, section, "vision_danger_section", section), !!m_client);
     }
     else
     {